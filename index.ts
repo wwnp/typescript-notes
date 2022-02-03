dc3d2ff6ce494aa68cfc89ab78a2cb41152ba11c
wwnp <iamtodaysad@gmail.com>
2022-02-03 10:09:15 +0300

    rf

--- index.ts
+++ index.ts
@@ -48,7 +48,7 @@ const tuple2: [string, boolean, number] = [true, 5, 5] //err
 //csv 
 type SimpleCSV = [string, string, number]
 const example: SimpleCSV[] = [
-  ['str', 'stasd', 32] 
+  ['str', 'stasd', 32]
 ]
 
 //OBJECT
@@ -70,7 +70,7 @@ const obj1: MyObjType = { a: 1, b: 2, c: "str", }
 //var 3 -------
 const obj2: object = { a: 1, b: 2, c: "str", }
 
-//var 4 -------
+// INTERFACES  -----------------------------------------------------
 interface MyObject {
   a: number;
   c: string;
@@ -85,7 +85,168 @@ const obj4: MyObject = { a: 1, c: '123', e: 'dqweqwe', 'sex': '123' } //err
 obj3.e = 5 //err
 
 
+// UNION INTERFACES -----------------------------------------------------
+interface Person {
+  name: string
+}
+interface Person {
+  age: number
+}
+const john: Person = {
+  name: 'John',
+  age: 23,
+}
+// добавить I (если назв интерфейсов совпадают)----
+interface IAccount {
+  login: string
+}
+const acc: IAccount = {
+  login: 'asdLOG'
+}
+// Union interface
+interface PersonDEV {
+  name: string
+}
+interface PersonDEV {
+  age: number
+}
+interface AccountDEV {
+  email: string
+  login: string
+  active: boolean
+}
 
+interface IDeveloper extends PersonDEV, AccountDEV {
+  skills: string[]
+  level?: string // ? необязательно
+}
+const johnDeveloper: IDeveloper = {
+  name: 'John',
+  age: 24,
+  email: 'string@qweq.ru',
+  login: 'johnLOG',
+  active: true,
+  skills: ['123', 'JS', 'CSS'],
+  // level: 'active',
+}
+// UNION TYPES------------------------------
+type TPerson = {
+  name: string;
+  age: number;
+}
+type TMYAccount = {
+  email: string
+  login: string
+  active: boolean
+}
+type TDeveloper = {
+  skills: string[]
+  level?: string // ? необязательно
+}
+type TFrontendDeveloper = TPerson & TMYAccount & TDeveloper // сложносоставной
+const frontEndDEVELOPRES: TFrontendDeveloper[] = [
+  {
+    name: 'front1',
+    age: 23,
+    email: 'string@dasd',
+    login: 'asdLOG',
+    active: true,
+    skills: ['asd', 'asd'],
+    // level?: '57' // ? необязательно
+  },
+  {
+    name: 'front2',
+    age: 23,
+    email: 'string@dasd',
+    login: 'asdLOG',
+    active: true,
+    skills: ['asd', 'asd'],
+    // level?: '57' // ? необязательно
+  },
+  {
+    name: 'front',
+    age: 23,
+    email: 'string@dasd',
+    login: 'asdLOG',
+    active: true,
+    skills: ['asd', 'asd'],
+    // level?: '57' // ? необязательно
+  }
+]
+// FUNCTION------------------------------
+const fn1 = (num: number): string => {
+  return String(num)
+}// get nubmer return string
+//------------
+type Callback = (num: number) => string
+function fn2(cb: Callback) {
+  return '123'
+}
+//------------
+function fn3(cb?: Callback) {
+  if (cb === undefined) {
+    cb = String
+  }
+  cb(12)
+}
+//------------
+function createPoint(x: number = 0, y: number = 0): [number, number] {
+  return [x, y]
+}
+createPoint(10)
+//------------
+function fn4(...nums: number[]): string {
+  return nums.join('-')
+}
+//------------
+interface Printable {
+  label: string
+}
+function printReport(obj: Printable): void {
+  console.log(obj.label);
+}// void when no return; 
+const drink = {
+  label: 'pepsi',
+  price: 90
+}
+const tel = {
+  label: 'huawei',
+  price: 123123
+}
+printReport(drink) // pepsi ; Printable in case fn work as Минимальный Сценарий, price can be 
+printReport(tel)  // huawei
+printReport({ label: 'Sex', price: '1230' }) // err 
+
+
+
+
+// ПЕРЕГРУЗКА ФУНКЦИЙ
+function pickCard0(x: number | { suit: string; card: number }[]): { suit: string; card: number } | number {
+  return 1
+}
+// or (better below)
+function pickCard(x: number): { suit: string; card: number }
+function pickCard(x: { suit: string; card: number }[]): number
+function pickCard(x): any {
+  if(typeof x === 'object'){
+    let pickedCard = Math.floor(Math.random() * x.length)
+    return pickedCard
+  }
+  else if(typeof x === 'number'){
+    let pickedCard
+    //...
+    return pickedCard
+  }
+}
+pickCard([{ suit: 'string', card: 123 }])
+pickCard(69)
+
+
+function test(a: number, b: string): {a:number}
+function test(a,b): any {
+  return a + parseInt(b)
+}
+test(1,'13')
 
 // Example:: First Lesson ------------------------------
 import axios from 'axios'
@@ -95,7 +256,7 @@ interface Todo {
   id: number
   title: string
   completed: boolean
-} // уточнить типы
+}
 axios.get(url).then(response => {
   const todo = response.data as Todo
   const id = todo.id
@@ -110,3 +271,102 @@ function logTodo(id: number, title: string, finished: boolean) {
     TODO finished ${finished}
     `);
 }
+
+// GENERICS- обощения
+const valueFactory = (x: number) => x;
+const myVal = valueFactory(11)
+
+type TypeFactory<X> = X;
+type MyType = TypeFactory<number>;
+// TypeFactory<number> ; in <> - generic (number) 
+
+// ---
+interface ValueCotainer<Value> {
+  value: Value
+}
+type StringContainer = ValueCotainer<string> // 
+const x: StringContainer = {
+  value: 'String'
+}
+//  ValueCotainer<string>; in <> - generic (string) 
+
+// example generic---
+class ArrayOfNums {
+  constructor(public collection: number[]) {
+    // this.collection = collection // don't need
+  }
+  get(index: number): number {
+    return this.collection[index]
+  }
+}
+class ArrayOfStrings {
+  constructor(public collection: string[]) {
+  }
+  get(index: string): string {
+    return this.collection[index]
+  }
+}
+// with generics (<T>)
+class ArrayOfAnything<T, K> {
+  constructor(public collection: T[]) {
+  }
+  get(index: number): T {
+    return this.collection[index]
+  }
+}
+new ArrayOfAnything<string, number>(['123', '456']) // <string,number> === <T, K>
+new ArrayOfAnything<number, number>([1, 2, 3])
+new ArrayOfAnything([31, 22, 33])
+
+// ----------
+function printString(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+function printNums(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+//with generics (<T>)
+function printAny<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+// printAny<number>([1, 2, 3])
+// printAny<string>(['123', '12322'])
+
+// -----
+function fillArray<T>(len: number, element: T) {
+  return new Array<T>(len).fill(element)
+}
+const a = fillArray<string>(5, '123')
+// console.log(a); // [ '123', '123', '123', '123', '123' ]
+
+// -----
+interface ArrayZ<T> {
+  concat(...items: Array<T[] | T>): T[]
+  // reduce<U>(
+  //   callback: (state: U, element: T, index: number, array: T[]) => U,
+  //   firstState?: U
+  // ): U
+}
+
+interface Test<T> {
+  test(name: T)
+}
+const t:Test<string> = {
+  test(name) {
+    return name
+  }
+}
+// console.log(t.test('sex'));
+type TypeFactory<T> = T;
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type head1 = First<arr1> // expected to be 'a'
+type head2 = First<arr2> // expected to be 3
